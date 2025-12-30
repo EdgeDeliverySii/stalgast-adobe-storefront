4c65b97709ac83d308ca9d332d744492b76b9ed6
Bartłomiej Jaruga <bjaruga@sii.pl>
2025-12-30 10:20:44 +0100

    feat: - Added business-ideas-custom block with styling - Added readme for business-ideas-custom block - standardized font-size for labels for featured-products-slider-custom and business-ideas-custom blocks

---
+++ blocks/business-ideas-custom/business-ideas-custom.js
@@ -0,0 +1,21 @@
+export default async function decorate(block) {
+  const sectionContainer = block.closest('div.section');
+  const sectionTitle = sectionContainer.querySelector('div > h2');
+  if(sectionTitle){
+    sectionTitle.classList.add('business-ideas-section-title');
+  }
+
+  const businessIdeaContainers = block.querySelectorAll(':scope > div');
+  businessIdeaContainers.forEach((businessIdeaContainer) => {
+    businessIdeaContainer.classList.add('business-idea-container');
+
+    const children = businessIdeaContainer.querySelectorAll(':scope > div');
+    if(children.length > 1){
+      const pictureContainer = children[0];
+      pictureContainer.classList.add('picture-container');
+
+      const labelContainer = children[1];
+      labelContainer.classList.add('label-container');
+    }
+  });
+}
